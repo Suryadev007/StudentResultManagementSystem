1d86201a96f65a3f60c1d48942757552a5762b48
Suryadev007 <107113316+Suryadev007@users.noreply.github.com>
2024-08-26 14:03:20 +0530

    publish srms to git account

---
+++ index.php
@@ -0,0 +1,145 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Faculty Login</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
+    <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
+    <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen">
+    <link rel="stylesheet" href="css/prism/prism.css" media="screen"> <!-- USED FOR DEMO HELP - YOU CAN REMOVE IT -->
+    <link rel="stylesheet" href="css/main.css" media="screen">
+    <script src="js/modernizr/modernizr.min.js"></script>
+</head>
+
+<body class="">
+    <div class="main-wrapper">
+
+        <div class="">
+            <div class="row">
+                <h1 align="center">Student Result Management System</h1>
+                <div class="col-lg-6 visible-lg-block">
+
+                    <section class="section">
+                        <div class="row mt-40">
+                            <div class="col-md-10 col-md-offset-1 pt-50">
+
+                                <div class="row mt-30 ">
+                                    <div class="col-md-11">
+                                        <div class="panel">
+                                            <div class="panel-heading">
+                                                <div class="panel-title text-center">
+                                                    <h4>For Students</h4>
+                                                </div>
+                                            </div>
+                                            <div class="panel-body p-20">
+
+
+                                                <form class="form-horizontal" method="post">
+                                                    <div class="form-group">
+                                                        <label for="inputEmail3" class="col-sm-6 control-label">Search
+                                                            your result</label>
+                                                        <div class="col-sm-6">
+                                                            <a href="find-result.php"><img src="images/stu.png" height="150px" width="150px"></a>
+
+                                                        </div>
+                                                    </div>
+
+                                                </form>
+
+
+
+
+                                            </div>
+                                        </div>
+                                     
+
+                                    </div>
+                                  
+                                </div>
+                            
+                            </div>
+                        
+                        </div>
+                  
+                    </section>
+                </div>
+                <div class="col-lg-6">
+                    <section class="section">
+                        <div class="row mt-40">
+                            <div class="col-md-10 col-md-offset-1 pt-50">
+
+                                <div class="row mt-30 ">
+                                    <div class="col-md-11">
+                                        <div class="panel">
+                                            <div class="panel-heading">
+                                                <div class="panel-title text-center">
+                                                    <h4>For Faculty</h4>
+                                                </div>
+                                            </div>
+                                            <div class="panel-body p-20">
+
+                                               
+
+                                                <form class="form-horizontal" method="post">
+                                                    <div class="form-group">
+                                                        <label for="inputEmail3" class="col-sm-6 control-label">Add
+                                                            Student result</label>
+                                                        <div class="col-sm-6">
+                                                            <a href="admin-login.php"><img src="images/tea.png" height="150px" width="150px"></a>
+
+                                                        </div>
+                                                    </div>
+                                                </form>
+
+
+
+
+                                            </div>
+                                        </div>
+                                    
+                                        
+                                    </div>
+                                  
+                                </div>
+                         
+                            </div>
+                           
+                        </div>
+                    
+                    </section>
+
+                </div>
+              
+            </div>
+          
+        </div>
+    
+
+    </div>
+  
+
+    
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/jquery-ui/jquery-ui.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/pace/pace.min.js"></script>
+    <script src="js/lobipanel/lobipanel.min.js"></script>
+    <script src="js/iscroll/iscroll.js"></script>
+
+    
+    <script src="js/main.js"></script>
+    <script>
+        $(function () {
+
+        });
+    </script>
+
+   
+</body>
+
+</html>
